91d2184875cf65e5128d1d105faeef8af79f31ba
bessam <sahlibessam@gmail.com>
2026-04-03 01:19:28 +0100

    enabling jacket by default

--- src/utils/imdb.ts
+++ src/utils/imdb.ts
@@ -1,34 +1,22 @@
 import axios from "axios";
 
-export const getTitle = async (imdbId: string, language?: string) => {
-  try {
-    const data = (
-      await axios.get(`https://www.imdb.com/title/${imdbId}`, {
-        headers: {
-          "Accept-Language": language,
-          "Accept-Encoding": "gzip,deflate,compress",
-          "User-Agent":
-            "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3",
-        },
-      })
-    ).data;
-
-    const title = data.match(/<title>(.*?)<\/title>/)[1] as string;
-    if (!title) return undefined;
-    return title.split(" (")[0];
-  } catch {
-    return undefined;
-  }
-};
+const CINEMETA_URL = "https://v3-cinemeta.strem.io/meta";
 
 export const getTitles = async (imdbId: string) => {
   const titles = new Set<string>();
 
-  (await Promise.all([getTitle(imdbId), getTitle(imdbId, "en")])).forEach(
-    (title) => {
-      if (title) titles.add(title);
-    }
+  const results = await Promise.all(
+    ["movie", "series"].map((type) =>
+      axios
+        .get(`${CINEMETA_URL}/${type}/${imdbId}.json`)
+        .then((res) => res.data?.meta?.name as string | undefined)
+        .catch(() => undefined)
+    )
   );
+
+  results.forEach((title) => {
+    if (title) titles.add(title);
+  });
 
   return [...titles];
 };
